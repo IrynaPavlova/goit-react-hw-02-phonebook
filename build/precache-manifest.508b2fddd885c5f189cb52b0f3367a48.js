254f4fd1005b653034a2c9d45994b2f7ea4a3421
Pavlova <irochkapavlova85@gmail.com>
2020-01-27 12:17:54 +0200

    ?

---
+++ build/precache-manifest.508b2fddd885c5f189cb52b0f3367a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a024e52b1d31edd812d9c8af48e94c18",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "cc95e45e8170392d8b69",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.cf5a41d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.cf5a41d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e277938abb676eaf3016",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.aeb7fe70.chunk.js"
+  },
+  {
+    "revision": "a22d6ae82fb1dc14f7fa",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.1ba8669c.js"
+  }
+]);
